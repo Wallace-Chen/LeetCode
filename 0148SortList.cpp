6cfd3f1a87268451fe442e486d75df2f9087e697
Wallace-Chen <32942703+Wallace-Chen@users.noreply.github.com>
2020-12-29 15:37:56 +0100

    Create 0148SortList.cpp

---
+++ 0148SortList.cpp
@@ -0,0 +1,143 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) { // merge sort, top-down method but with O(lgn) space. See below for down-top method withh constant space
+        if(!head || !head->next) return head;
+        ListNode *fast=head, *slow = head;
+        while(fast->next && fast->next->next){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        /*
+        if(slow == fast){ // list has two nodes
+            if(slow->val > slow->next->val){
+                fast = fast->next;
+                slow->next->next = slow;
+                slow->next = nullptr;
+                return fast;
+            }
+            else return head;
+        }
+        */
+        fast = slow->next; // the head of the second half
+        slow->next = nullptr;
+        slow = head; // the head of the first half
+        slow = sortList(slow);
+        fast = sortList(fast);
+        //merge part
+        // the first half is longer or equal than the second half in length
+        ListNode *dummy = new ListNode(), *curr = dummy;
+        while(slow && fast){
+            ListNode *tmp;
+            if(slow->val < fast->val){
+                curr->next = slow;
+                slow = slow->next;
+            }else{
+                curr->next = fast;
+                fast = fast->next;
+            }
+            curr = curr->next;
+        }
+        if(slow) curr->next = slow;
+        else curr->next = fast;
+        return dummy->next;
+    }
+};
+
+// Copied from official solution, not implemented yet
+// https://leetcode.com/problems/sort-list/solution/
+/*
+
+class Solution {
+public:
+    ListNode* tail = new ListNode();
+    ListNode* nextSubList = new ListNode();
+
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head -> next)
+            return head;
+        int n = getCount(head);
+        ListNode* start = head;
+        ListNode dummyHead(0);
+        for (int size = 1; size < n; size = size * 2) {
+            tail = &dummyHead;
+            while (start) {
+                if (!start->next) {
+                    tail->next = start;
+                    break;
+                }
+                ListNode* mid = split(start, size);
+                merge(start, mid);
+                start = nextSubList;
+            }
+            start = dummyHead.next;
+        }
+        return dummyHead.next;
+    }
+
+    ListNode* split(ListNode* start, int size) {
+        ListNode* midPrev = start;
+        ListNode* end = start->next;
+        //use fast and slow approach to find middle and end of second linked list
+        for (int index = 1; index < size && (midPrev->next || end->next); index++) {
+            if (end->next) {
+                end = (end->next->next) ? end->next->next : end->next;
+            }
+            if (midPrev->next) {
+                midPrev = midPrev->next;
+            }
+        }
+        ListNode* mid = midPrev->next;
+        nextSubList = end->next;
+        midPrev->next = nullptr;
+        end->next = nullptr;
+        // return the start of second linked list
+        return mid;
+    }
+
+    void merge(ListNode* list1, ListNode* list2) {
+        ListNode dummyHead(0);
+        ListNode* newTail = &dummyHead;
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                newTail->next = list1;
+                list1 = list1->next;
+                newTail = newTail->next;
+            } else {
+                newTail->next = list2;
+                list2 = list2->next;
+                newTail = newTail->next;
+            }
+        }
+        newTail->next = (list1) ? list1 : list2;
+        // traverse till the end of merged list to get the newTail
+        while (newTail->next) {
+            newTail = newTail->next;
+        }
+        // link the old tail with the head of merged list
+        tail->next = dummyHead.next;
+        // update the old tail with the new tail of merged list
+        tail = newTail;
+    }
+
+    int getCount(ListNode* head) {
+        int cnt = 0;
+        ListNode* ptr = head;
+        while (ptr) {
+            ptr = ptr->next;
+            cnt++;
+        }
+        return cnt;
+    }
+};
+
+*/
